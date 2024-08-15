83efaa0bfd5e1d575b41a189b5e6d78c7e46177f
BIENNHAT <nhatbien2003@gmail.com>
2024-08-15 22:31:17 +0700

    video 66(#55): Hoàn Thiện Menu Phân Quyền Dành Cho Admin

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -8,13 +8,14 @@ import Navigator from '../../components/Navigator';
 import { adminMenu } from './menuApp';
 import './Header.scss';
 import { LANGUAGES } from '../../utils';
+import { FormattedMessage } from 'react-intl';
 
 class Header extends Component {
     handleChangeLanguage = (language) => {
         this.props.changeLanguageAppRedux(language);
     }
     render() {
-        const { processLogout, language } = this.props;
+        const { processLogout, language, userInfo } = this.props;
 
         return (
             <div className="header-container">
@@ -24,6 +25,10 @@ class Header extends Component {
                 </div>
 
                 <div className='languages'>
+                    <span className='welcome'>
+                        <FormattedMessage id='homeheader.welcome' />
+                        {userInfo && userInfo.firstName ? userInfo.firstName : ''} !
+                    </span>
                     <span
                         className={language === LANGUAGES.VI ? 'language-vi active' : 'language-vi'}
                         onClick={() => this.handleChangeLanguage(LANGUAGES.VI)}
@@ -53,6 +58,7 @@ const mapStateToProps = state => {
     return {
         isLoggedIn: state.user.isLoggedIn,
         language: state.app.language,
+        userInfo: state.user.userInfo
     };
 };
 
--- src/containers/Header/menuApp.js
+++ src/containers/Header/menuApp.js
@@ -1,13 +1,47 @@
 export const adminMenu = [
-    { //hệ thống
-        name: 'menu.system.header', menus: [
+    { // Quản lý người dùng
+        name: 'menu.admin.manage-user', 
+        menus: [
             {
-                name: 'menu.system.system-administrator.header',
-                subMenus: [
-                    { name: 'menu.system.system-administrator.user-manage', link: '/system/user-manage' },
-                    { name: 'menu.system.system-administrator.user-redux', link: '/system/user-redux' },
-                ]
-            },
+                name: 'menu.admin.crud', link: '/system/user-manage' // CRUD User theo state của React (chưa dùng Redux)
+            }, 
+            {
+                name: 'menu.admin.crud-redux', link: '/system/user-redux' // CRUD theo Redux
+            }, 
+            {
+                name: 'menu.admin.manage-doctor', link: '/system/user-doctor'
+            }, 
+            {
+                name: 'menu.admin.manage-admin', link: '/system/user-admin'
+            }, 
+                
         ]
-    },
+    }, 
+    { // Quản lý phòng khám
+        name: 'menu.admin.clinic', 
+        menus: [
+            {
+                name: 'menu.admin.manage-clinic', link: '/system/manage-clinic' // CRUD User theo state của React (chưa dùng Redux)
+            }, 
+
+        ]
+    }, 
+    { // Quản lý chuyên khoa
+        name: 'menu.admin.specialty', 
+        menus: [
+            {
+                name: 'menu.admin.manage-specialty', link: '/system/manage-specialty' // CRUD User theo state của React (chưa dùng Redux)
+            }, 
+
+        ]
+    }, 
+    { // Quản lý cẩm nang
+        name: 'menu.admin.handbook', 
+        menus: [
+            {
+                name: 'menu.admin.manage-handbook', link: '/system/manage-handbook' // CRUD User theo state của React (chưa dùng Redux)
+            }, 
+
+        ]
+    }, 
 ];
